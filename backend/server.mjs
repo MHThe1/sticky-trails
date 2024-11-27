3c5c64a2dad6d2817aca5c9d4ace1a3df47b1594
Mehedi Hasan <mehedihtanvir@gmail.com>
2024-11-27 16:14:15 +0600

    delete method created

--- backend/server.mjs
+++ backend/server.mjs
@@ -31,6 +31,18 @@ app.post("/api/notes", async (req, res) => {
   }
 });
 
+app.delete("/api/notes/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await Note.findByIdAndDelete(id);
+    res.status(200).json({success: true, message: "Note deleted successfully"});
+  } catch (error) {
+    console.error("Error deleting note:", error.message);
+    res.status(500).json({success: false, message: "Error deleting note" });
+  }
+});
+
 app.listen(5000, () => {
   connectDB();
   console.log("Server is running on port 5000 hit http://localhost:5000/");
